a9b5f5ad46ec62595573f0d8ebb983db0fb19576
Luke Greenwood <lukegreenwood@Lukes-MacBook-Pro.local>
2019-11-11 22:31:56 -0800

    bars work

--- evkitty/js/game.js
+++ evkitty/js/game.js
@@ -1,5 +1,6 @@
 console.log("hello world!");
-var healthCounter = 0;
+var happyCounter = 50;
+var chonkCounter = 30;
 const textElement = document.getElementById('gametext');
 const optionButttonsElement = document.getElementById('optionsContainer');
 
@@ -31,10 +32,11 @@ function showOption(option) {
 
 function selectOption (option) {
   const nextTextNodeId = option.nextText
-  healthCounter += option.food
-  // health.innerText = healthCounter
-  pb1.setValue(healthCounter)
   showTextNode(nextTextNodeId)
+  happyCounter += option.happy
+  pb2.setValue(happyCounter)
+  chonkCounter += option.chonk
+  pb1.setValue(chonkCounter)
 }
 
 const textNodes = [
@@ -45,16 +47,20 @@ const textNodes = [
       {
         text: '< Aquire food >',
         nextText: 2,
-        food:20
+        happy:20,
+        chonk: 0
       },
       {
         text: '< Aquire cuddles >',
         nextText: 3,
-        food:-10
+        happy:-10,
+        chonk: 0,
       },
       {
         text: '< Sleep >',
         nextText: 4,
+        happy: 0,
+        chonk: 0
       }
     ]
   },
@@ -65,7 +71,7 @@ const textNodes = [
       {
         text: '< Aquire cuddles >',
         nextText: 1,
-        food:-10
+        happy:-10
       }
     ]
   }
@@ -76,11 +82,11 @@ startGame();
 
 
 
-
-class ProgressBar {
+// chonk level
+class ProgressBar1 {
   constructor (element, initialValue = 0) {
-    this.valueElem = element.querySelector(".progress-bar-value");
-    this.fillElem = element.querySelector(".progress-bar-fill");
+    this.valueElem = element.querySelector(".progress-bar-value1");
+    this.fillElem = element.querySelector(".progress-bar-fill1");
 
     this.setValue(initialValue);
   }
@@ -101,10 +107,44 @@ class ProgressBar {
     const percentage = this.value + "%";
 
     this.fillElem.style.width = percentage;
-    this.valueElem.textContent = percentage;
+    this.valueElem.textContent = "Chonk-iness: " + percentage;
   }
 
 }
 
-const pb1 = new ProgressBar(document.querySelector(".progress-bar"), 75);
-pb1.setValue(healthCounter);
+const pb1 = new ProgressBar1(document.querySelector(".progress-bar1"), 0);
+pb1.setValue(chonkCounter);
+
+
+// happiness meter
+class ProgressBar2 {
+  constructor (element, initialValue = 0) {
+    this.valueElem = element.querySelector(".progress-bar-value2");
+    this.fillElem = element.querySelector(".progress-bar-fill2");
+
+    this.setValue(initialValue);
+  }
+
+  setValue (newValue) {
+    if (newValue < 0) {
+      newValue = 0;
+    }
+    if (newValue > 100) {
+      newValue = 100;
+    }
+
+    this.value = newValue;
+    this.update();
+  }
+
+  update () {
+    const percentage = this.value + "%";
+
+    this.fillElem.style.width = percentage;
+    this.valueElem.textContent = "Happiness: " + percentage;
+  }
+
+}
+
+const pb2 = new ProgressBar2(document.querySelector(".progress-bar2"), 0);
+pb2.setValue(happyCounter);
