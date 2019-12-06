2063e34a21f75acd4c853711e4e34d7aa4e911f0
lagreenw <lagreenw@usc.edu>
2019-12-06 14:58:18 -0800

    click drag new balls works!!

--- final/sketch.js
+++ final/sketch.js
@@ -5,6 +5,7 @@ var marginX;
 var marginY;
 var balls = [];
 var ballButton;
+var draggingNewBall = false;
 
 function preload () {
 
@@ -14,6 +15,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   gridSize = (width -200) / numberOfSqr;
   balls[0]  = new Ball(4, 4);
+  //create buttons
+  ballButton = new BallButton();
 }
 
 
@@ -32,10 +35,12 @@ function draw() {
       rect(100 + x*gridSize, 200 + y*gridSize, gridSize, gridSize);
     }
   }
-  //draw buttons
-  ballButton = new BallButton();
+  //check buttons
   ballButton.hover();
   ballButton.show();
+  if (draggingNewBall){
+    balls[balls.length - 1].moveOnMouseDuringNewDrag();
+  }
   //show balls & balls interaction
   for (var i = 0; i < balls.length; i++) {
     balls[i].show()
@@ -51,16 +56,28 @@ function mousePressed(){
   for (var i = 0; i < balls.length; i++) {
     balls[i].mouseOverBall();
   }
-  ballButton.mouseOverButton();
+  //initiate draggingNewBall
+  if (dist(mouseX, mouseY, ballButton.x, ballButton.y) < ballButton.d/2){
+    draggingNewBall = true;
+    let b = new Ball(mouseX, mouseY);
+    balls.push(b);
+  } else {
+    draggingNewlBall = false;
+  }
 }
 
 function mouseReleased(){
   for (var i = 0; i < balls.length; i++) {
     balls[i].release();
   }
+  if (draggingNewBall == true){
+    draggingNewBall = false;
+    let grid = calculateGridPos(mouseX, mouseY);
+    let pos = calculateXY(grid.x, grid.y)
+    balls[balls.length - 1].x = pos.x;
+    balls[balls.length - 1].y = pos.y;
+  }
 }
-
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -113,13 +130,11 @@ class Ball {
     this.yGrid = tempY;
     this.dragging = false;
   }
-
   mouseOverBall(){
     if(dist(mouseX, mouseY, this.x, this.y) < gridSize/2){
       this.dragging = true;
     }
   }
-
   release(){
     if(this.dragging){
       this.dragging = false;
@@ -129,13 +144,15 @@ class Ball {
       this.y = pos.y;
     }
   }
-
   moveOnMouse(){
     if(this.dragging){
       this.x = mouseX;
       this.y = mouseY;
     }
-
+  }
+  moveOnMouseDuringNewDrag(){
+    this.x = mouseX;
+    this.y = mouseY;
   }
   show(){
     fill(0);
@@ -152,21 +169,13 @@ class BallButton {
     this.y = 100;
     this.d = 50;
     this.color = 0;
-    this.dragging = false;
   }
-  mouseOverButton(){
-    if(dist(mouseX, mouseY, this.x, this.y) < this.d/2){
-      balls.push(new Ball());
-      balls[balls.length - 1].x = this.x;
-      balls[balls.length - 1].y = this.y;
-      print(this.dragging);
-    }
-  }
-
   hover() {
     var d = dist(this.x, this.y, mouseX, mouseY);
     if (d < this.d/2) {
       this.color = 50;
+    } else {
+      this.color = 0;
     }
   }
   show() {
